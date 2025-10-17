c26fbfbf2906a70dd8f02e6b6dac78aac2ddd835
Ekaterina Biryukova <dorekv@gmail.com>
2025-10-17 18:20:32 +0300

    Use pipes for formatting product price.

---
+++ src/app/pipes/discount-display-pipe.spec.ts
@@ -0,0 +1,8 @@
+import { DiscountDisplayPipe } from './discount-display-pipe';
+
+describe('DiscountDisplayPipe', () => {
+  it('create an instance', () => {
+    const pipe = new DiscountDisplayPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
