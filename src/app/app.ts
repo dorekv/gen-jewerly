6fff982b26777d8cbc24a187e2f525814e1119a2
Ekaterina Biryukova <dorekv@gmail.com>
2025-11-12 17:58:10 +0200

    Add active menu styling.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  standalone: true, // <-- важно
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
-
 export class App {
   protected readonly title = signal('gen-jewerly');
 
